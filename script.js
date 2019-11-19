f688bbd50cb4313ac7e4aca6e8cedacd51e781ea
Kat Powell <thekatnes@gmail.com>
2019-11-19 10:06:07 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,71 @@
+/* Welcome to my JS Sandbox Library. This is where my playground is for learning more about javaScript and working towards my 2020 goal of becoming a stronger Web Developer who specializes in scripting <3 */
+
+//----------------------------------------------------------
+
+// Strings Appear Black in DOM, Numbers and Bools are blue
+// console.log("Hello");
+// console.log(123);
+// console.log(true);
+
+//----------------------------------------------------------
+
+// Logging variables, arrays, objects, tables, errors and warnings
+// var greeting = "Hello there!";
+// console.log(greeting);
+// console.log([1,2,3,4]);
+// console.log({a:1, b:2});
+// console.table({a:1, b:2});
+// console.error("This is an error");
+// console.warn("This is a warning");
+
+//----------------------------------------------------------
+
+//Time for how long it takes for commands within .time to run
+// console.time("Greeting");
+//   console.log("yo");
+//   console.log("yo");
+//   console.log("yo,");
+//   console.log("it's");
+//   console.log("me,");
+//   console.log("Kat");
+// console.timeEnd("Greeting");
+
+//----------------------------------------------------------
+
+//Type Conversion | Number to a String
+// let val;
+// val = 5; 
+// console.log(val);
+// console.log(typeof val)
+// val = String(5);
+// console.log(val);
+// console.log(typeof val);
+
+//Type Conversion | Boolean to a String
+// let car;
+// car = true;
+// console.log(car);
+// console.log(typeof car);
+// car = String(car);
+// console.log(car);
+// console.log(typeof car);
+
+//----------------------------------------------------------
+
+//More Exercises with object properties and methods
+// let name = "Michael"
+// let firstName = name.toUpperCase();
+// console.log(firstName);
+
+// firstName = name.toLowerCase();
+// console.log(firstName);
+
+// firstName = name.length;
+// console.log(firstName);
+
+// let lastChar = name.charAt(name.length - 1);
+// console.log(lastChar);
+
+
+
+
